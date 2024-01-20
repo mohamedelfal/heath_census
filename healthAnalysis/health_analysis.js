25aaeb5c52bc493915882d4d1dd81cb42078174f
Mohamed Antar Abdelhadi <100445644+mohamedelfal@users.noreply.github.com>
2024-01-21 01:53:07 +0200

    Update health_analysis.js

--- healthAnalysis/health_analysis.js
+++ healthAnalysis/health_analysis.js
@@ -2,3 +2,17 @@
     const report = document.getElementById("report");
     const btnSearch = document.getElementById('btnSearch');
     const patients = [];
+
+     function addPatient() {
+          const name = document.getElementById("name").value;
+          const gender = document.querySelector('input[name="gender"]:checked');
+          const age = document.getElementById("age").value;
+          const condition = document.getElementById("condition").value;
+
+          if (name && gender && age && condition) {
+            patients.push({ name, gender: gender.value, age, condition });
+            resetForm();
+            generateReport();
+          }
+        }
+
