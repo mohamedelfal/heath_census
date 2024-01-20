75eb1f407083ac354866dcdc0185a659fd2eae5e
Mohamed Antar Abdelhadi <100445644+mohamedelfal@users.noreply.github.com>
2024-01-21 01:47:25 +0200

    Update health_analysis.js

--- healthAnalysis/health_analysis.js
+++ healthAnalysis/health_analysis.js
@@ -1,1 +1,4 @@
-
+    const addPatientButton = document.getElementById("addPatient");
+    const report = document.getElementById("report");
+    const btnSearch = document.getElementById('btnSearch');
+    const patients = [];
